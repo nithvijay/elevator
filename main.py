d912ab078a833222578840c6a2069b74cda82586
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-04-29 20:44:34 -0400

    updated comments

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from elevator import Elevator, ElevatorRunner
 from time import time
 import numpy as np
 
-t1 = time()
-#np.random.seed(0)
-e1 = Elevator(num_floors=10, default_floor=1)
-er1 = ElevatorRunner([e1], animation=True)
-er1.simulate(1000, .05)
-#from animation import make_plot
-
-#make_plot(er1.elevator_states, save=False)
-
-# print(er1.elevator_states)
-print(time() - t1)
-pass
+if __name__ == "__main__":
+    t1 = time()
+    #np.random.seed(0)
+    e1 = Elevator(num_floors=10, default_floor=1)
+    er1 = ElevatorRunner([e1], animation=True)
+    er1.simulate(1000, .05)
+    #from animation import make_plot
+    #make_plot(er1.elevator_states, save=False)
+    # print(er1.elevator_states)
+    print(time() - t1)
+    pass
